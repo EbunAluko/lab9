69f085cad9c332a5c779b0f690ed2bcb88b0aa14
EbunAluko <98724992+EbunAluko@users.noreply.github.com>
2024-12-13 15:00:03 -0500

    Update monopolyService.js

--- monopolyService.js
+++ monopolyService.js
@@ -30,6 +30,7 @@
 const pgp = require('pg-promise')();
 
 const db = pgp({
+  ssl: { rejectUnauthorized: false, },
   host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
